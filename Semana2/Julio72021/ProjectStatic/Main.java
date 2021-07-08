0d66bb243dd3a39c21ac6c13c8258971ee1a296f
danieljpadilla2011 <85798282+danieljpadilla2011@users.noreply.github.com>
2021-07-07 21:11:42 -0500

    Create Main.java

---
+++ Semana2/Julio72021/ProjectStatic/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("El radio es: " + Circulo.PI);
+        System.out.println("El area es: " + Circulo.calcularArea(6));
+
+    }
+
+}
