246790eda4a6bcb0dca2263073802e1f79f3615b
danieljpadilla2011 <85798282+danieljpadilla2011@users.noreply.github.com>
2021-07-15 23:12:35 -0500

    Add files via upload

---
+++ Semana2/Julio82021/ProjecAbstracta/src/FiguraGeometrica.java
@@ -0,0 +1,15 @@
+public abstract class FiguraGeometrica {
+    private int numeroLados;
+
+    public FiguraGeometrica(int numeroLados) {
+        this.numeroLados = numeroLados;
+    }
+
+    // public void mostrarNumeroLados() {
+    // System.out.println("El numero de lados es: " + numeroLados);
+    // }
+
+    public abstract float area();
+
+    public abstract float perimetro();
+}
---
+++ Semana2/Julio82021/ProjecAbstracta/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Cuadrado cuadrado1 = new Cuadrado(4, 15);
+        System.out.println("El area del cuadrado es: " + cuadrado1.area());
+        System.out.println("El perimetro del cuadrado es: " + cuadrado1.perimetro());
+    }
+
+}
